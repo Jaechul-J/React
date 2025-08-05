29aaac3915f8f845104ec1a5947aed96136405c5
Jaechul Jung <jaechul@jeongjaecheols-MacBook-Air.local>
2025-08-05 16:48:57 +0900

    Completed React Essentials

---
+++ Documents/Projects/React/01-starting-project/src/App.jsx
@@ -0,0 +1,44 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { CORE_CONCEPTS } from "./data";
+
+function App() {
+  let tabContent = "Please click a button!";
+
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+    tabContent = selectedButton;
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Pros</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
