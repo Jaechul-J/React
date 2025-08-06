924645d7bcd35fc0a57029697daf77c9e7d85554
Jaechul Jung <jaechul@jeongjaecheols-MacBook-Air.local>
2025-08-06 14:19:58 +0900

    Rearranged components neat and nice

--- Documents/Projects/React/01-starting-project/src/App.jsx
+++ Documents/Projects/React/01-starting-project/src/App.jsx
@@ -1,41 +1,14 @@
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { CORE_CONCEPTS } from "./data";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  let tabContent = "Please click a button!";
-
-  function handleSelect(selectedButton) {
-    console.log(selectedButton);
-    tabContent = selectedButton;
-  }
-
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Pros</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
